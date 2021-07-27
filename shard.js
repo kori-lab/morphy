2e8cb1039673a40f346234a55a8bc074e1874300
akirou-lab <prxffadeus@gmail.com>
2021-07-27 01:34:29 -0300

    docs(Morphy): add client options comment

---
+++ shard.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const { Logs } = require('./src');
+const { ShardingManager } = require('discord.js');
+
+const manager = new ShardingManager('./index.js', {
+  token: process.env.DISCORD_TOKEN,
+  totalShards: process.env.SHARD_AMOUNT || 'auto',
+});
+
+manager.spawn();
+
+manager.on('shardCreate', shard => {
+  Logs.shardLog(`O shard com o id ${shard.id} esta sendo inicializado...`);
+  shard.on('ready', () => {
+    Logs.shardLog(`O shard com o id ${shard.id} foi inicializado com sucesso!`);
+  });
+});
