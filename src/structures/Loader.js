ce4ab14a54f1a5f66fdf9c34860f95b3b3d7a936
Sasuke-7 <davidkunschventurini@gmail.com>
2021-07-28 21:05:45 -0300

    feat(Structures): added loader structure

---
+++ src/structures/Loader.js
@@ -0,0 +1,27 @@
+const Morphy = require('../Morphy');
+
+module.exports = class Loader {
+  /**
+   * @typedef LoaderOptions
+   * @prop {?boolean} preLoad If the loader needs to load before all loaders
+   * @prop {?boolean} important Break the client if the loader can't load
+   */
+
+  /**
+   * The loader structure to extends every loaders that will be created
+   * @param {Morphy} client The client to execute the loader
+   * @param {LoaderOptions} options The loader options
+   * @extends
+   */
+  constructor(client, options) {
+    this.client = client;
+    this.preLoad = !!options?.preLoad;
+    this.important = !!options?.important;
+  }
+
+  /**
+   * The loader start function
+   * @extends
+   */
+  async start() {}
+};
