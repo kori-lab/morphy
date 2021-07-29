e6b98873aa52b11d26e811617dce520615b294b3
akirou-lab <prxffadeus@gmail.com>
2021-07-29 20:01:46 -0300

    feat(Avatar): add avatar command

--- src/commands/utils/Avatar.js
+++ src/commands/utils/Avatar.js
@@ -1,18 +1,68 @@
-const { Message } = require('discord.js');
+const {
+  Message,
+  User,
+  MessageEmbed,
+  MessageActionRow,
+  MessageButton,
+} = require('discord.js');
 const { Command } = require('../..');
 
 module.exports = class extends Command {
   constructor(client) {
     super(client, {
-      name: 'ping',
-      aliases: ['ms', 'latencia'],
+      name: 'avatar',
+      aliases: ['a', 'foto'],
+      options: [
+        {
+          type: 'USER',
+          required: false,
+        },
+      ],
     });
   }
 
   /**
    * @param {Message} message
+   * @param {[User]} args
    */
-  async run(message) {
-      
+  async run(message, [user]) {
+    const { author } = message;
+    const target = user ?? author;
+
+    const targetAvatar = target.displayAvatarURL({ format: 'png', dynamic: true });
+
+    const msg = await message
+      .reply({
+        embeds: [
+          new MessageEmbed()
+            .setDescription(`\\✨ Avatar de **${target.tag}**`)
+            .setImage(targetAvatar)
+            .setColor('#313131')
+            .setFooter(
+              author.username,
+              author.displayAvatarURL({ format: 'png', dynamic: true })
+            )
+            .setTimestamp(),
+        ],
+        components: [
+          new MessageActionRow().addComponents(
+            new MessageButton().setEmoji('🗑').setCustomId('delete').setStyle('DANGER'),
+
+            new MessageButton().setEmoji('🖼').setURL(targetAvatar).setStyle('LINK')
+          ),
+        ],
+      })
+      .catch(() => null);
+
+    const filter = interaction => interaction.user.id === author.id;
+    await msg
+      .awaitMessageComponent({
+        filter,
+        time: 4000,
+      })
+      .catch(() => null);
+
+    message.delete().catch(() => null);
+    msg.delete().catch(() => null);
   }
-}
+};
