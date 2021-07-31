fdb583f24805426e036eed0d70803e38c13e4022
Sasuke <56622580+Sasuke-7@users.noreply.github.com>
2021-07-31 01:07:33 -0300

    feat(Ping): removed emoji from message
    Co-authored-by: sayanscript <77183715+sayanscript@users.noreply.github.com>

--- src/commands/utils/Ping.js
+++ src/commands/utils/Ping.js
@@ -27,7 +27,7 @@ module.exports = class extends Command {
       const ms = msg.createdAt - message.createdAt;
       await msg
         .edit({
-          embeds: [embed.setDescription(`🏓 Pong! \`${ms}ms\``)],
+          embeds: [embed.setDescription(` Pong! \`${ms}ms\``)],
         })
         .catch(() => null);
     }
